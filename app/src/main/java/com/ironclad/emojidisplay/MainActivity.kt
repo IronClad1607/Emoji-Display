45a8ac03274160c1e3c46391072792c2eda0c6c2
Ishaan Jain <ishaan160799@gmail.com>
2020-08-22 21:08:31 +0530

    get emoji function done!

--- app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
+++ app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ironclad.emojidisplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val smileEmojiUnicode = 0x1F601
+
+        val smileEmojiString = getEmoji(smileEmojiUnicode)
+        Log.d("PUI", smileEmojiString)
+        tvText.text = smileEmojiString
+    }
+
+    private fun getEmoji(unicode: Int): String {
+        return String(Character.toChars(unicode))
     }
 }
