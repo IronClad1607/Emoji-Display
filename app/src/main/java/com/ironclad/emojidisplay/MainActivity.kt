1e9cb440cc6c23fab9c25719cf5dbd982d714ecf
Ishaan Jain <ishaan160799@gmail.com>
2020-08-22 22:28:02 +0530

    all emojis placed

--- app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
+++ app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
@@ -30,7 +30,29 @@ class MainActivity : AppCompatActivity() {
         val hundredEmojiUnicode = 0x1F4AF
         val fireEmojiUnicode = 0x1F525
         val sleepingEmojiUnicode = 0x1F634
-        val heartEmojiUnicode = 0xFE0F;
+        val heartEmojiUnicode = 0x2764
+
+        tvSmile.text = getEmoji(smileEmojiUnicode)
+        tvLaugh.text = getEmoji(laughEmojiUnicode)
+        tvWink.text = getEmoji(winkEmojiUnicode)
+        tvTongueOut.text = getEmoji(tongueOutEmojiUnicode)
+        tvEyeLove.text = getEmoji(eyeLoveEmojiUnicode)
+        tvTaunt.text = getEmoji(tauntEmojiUnicode)
+        tvSeriousTaunt.text = getEmoji(seriousTauntEmojiUnicode)
+        tvKiss.text = getEmoji(kissEmojiUnicode)
+        tvAngry.text = getEmoji(angryEmojiUnicode)
+        tvFine.text = getEmoji(fineEmojiUnicode)
+        tvShock.text = getEmoji(shockEmojiUnicode)
+        tvShy.text = getEmoji(shyEmojiUnicode)
+        tvEarth.text = getEmoji(earthEmojiUnicode)
+        tvPumpkin.text = getEmoji(pumpkinEmojiUnicode)
+        tvCrying.text = getEmoji(cryingEmojiUnicode)
+        tvOneEyeTear.text = getEmoji(oneEyeTearEmojiUnicode)
+        tvMovie.text = getEmoji(movieEmojiUnicode)
+        tvHundred.text = getEmoji(hundredEmojiUnicode)
+        tvFire.text = getEmoji(fireEmojiUnicode)
+        tvSleeping.text = getEmoji(sleepingEmojiUnicode)
+        tvHeart.text = getEmoji(heartEmojiUnicode)
     }
 
     private fun getEmoji(unicode: Int): String {
