07cf9b7146733d6e1f5c4a154efec2129024ec02
Ishaan Jain <ishaan160799@gmail.com>
2020-08-22 21:37:14 +0530

    tongue out, eye love ,taunt, serious taunt, kiss, angry, fine emoji added

--- app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
+++ app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
@@ -24,6 +24,53 @@ class MainActivity : AppCompatActivity() {
         val winkEmojiString = getEmoji(winkEmojiUnicode)
         Log.d("PUI", winkEmojiString)
         tvWink.text = winkEmojiString
+
+        val tongueOutEmojiUnicode = 0x1F60B
+        val tougueOutEmojiString = getEmoji(tongueOutEmojiUnicode)
+        Log.d("PUI", tougueOutEmojiString)
+        tvTongueOut.text = tougueOutEmojiString
+
+        val eyeLoveEmojiUnicode = 0x1F60D
+        val eyeLoveEmojiString = getEmoji(eyeLoveEmojiUnicode)
+        Log.d("PUI", eyeLoveEmojiString)
+        tvEyeLove.text = eyeLoveEmojiString
+
+        val tauntEmojiUnicode = 0x1F60F
+        val tauntEmojiString = getEmoji(tauntEmojiUnicode)
+        Log.d("PUI", tauntEmojiString)
+        tvTaunt.text = tauntEmojiString
+
+        val seriousTauntEmojiUnicode = 0x1F612
+        val seriousTauntEmojiString = getEmoji(seriousTauntEmojiUnicode)
+        Log.d("PUI", seriousTauntEmojiString)
+        tvSeriousTaunt.text = seriousTauntEmojiString
+
+        val kissEmojiUnicode = 0x1F618
+        val kissEmojiString = getEmoji(kissEmojiUnicode)
+        Log.d("PUI", kissEmojiString)
+        tvKiss.text = kissEmojiString
+
+        val angryEmojiUnicode = 0x1F624
+        val angryEmojiString = getEmoji(angryEmojiUnicode)
+        Log.d("PUI", angryEmojiString)
+        tvAngry.text = angryEmojiString
+
+        val fineEmojiUnicode = 0x1F62A
+        val fineEmojiString = getEmoji(fineEmojiUnicode)
+        Log.d("PUI", fineEmojiString)
+        tvFine.text = fineEmojiString
+
+
+        val winkEmojiUnicode = 0x1F609
+        val winkEmojiString = getEmoji(winkEmojiUnicode)
+        Log.d("PUI", winkEmojiString)
+        tvWink.text = winkEmojiString
+
+
+        val winkEmojiUnicode = 0x1F609
+        val winkEmojiString = getEmoji(winkEmojiUnicode)
+        Log.d("PUI", winkEmojiString)
+        tvWink.text = winkEmojiString
     }
 
     private fun getEmoji(unicode: Int): String {
