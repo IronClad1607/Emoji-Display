1892497ce8772cbd84d536dbb60ae8ef916ef683
Ishaan Jain <ishaan160799@gmail.com>
2020-08-22 22:13:17 +0530

    restruct done

--- app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
+++ app/src/main/java/com/ironclad/emojidisplay/MainActivity.kt
@@ -11,66 +11,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val smileEmojiUnicode = 0x1F601
-        val smileEmojiString = getEmoji(smileEmojiUnicode)
-        Log.d("PUI", smileEmojiString)
-        tvSmile.text = smileEmojiString
-
         val laughEmojiUnicode = 0x1F602
-        val laughEmojiString = getEmoji(laughEmojiUnicode)
-        Log.d("PUI", laughEmojiString)
-        tvLaugh.text = laughEmojiString
-
         val winkEmojiUnicode = 0x1F609
-        val winkEmojiString = getEmoji(winkEmojiUnicode)
-        Log.d("PUI", winkEmojiString)
-        tvWink.text = winkEmojiString
-
         val tongueOutEmojiUnicode = 0x1F60B
-        val tougueOutEmojiString = getEmoji(tongueOutEmojiUnicode)
-        Log.d("PUI", tougueOutEmojiString)
-        tvTongueOut.text = tougueOutEmojiString
-
         val eyeLoveEmojiUnicode = 0x1F60D
-        val eyeLoveEmojiString = getEmoji(eyeLoveEmojiUnicode)
-        Log.d("PUI", eyeLoveEmojiString)
-        tvEyeLove.text = eyeLoveEmojiString
-
         val tauntEmojiUnicode = 0x1F60F
-        val tauntEmojiString = getEmoji(tauntEmojiUnicode)
-        Log.d("PUI", tauntEmojiString)
-        tvTaunt.text = tauntEmojiString
-
         val seriousTauntEmojiUnicode = 0x1F612
-        val seriousTauntEmojiString = getEmoji(seriousTauntEmojiUnicode)
-        Log.d("PUI", seriousTauntEmojiString)
-        tvSeriousTaunt.text = seriousTauntEmojiString
-
         val kissEmojiUnicode = 0x1F618
-        val kissEmojiString = getEmoji(kissEmojiUnicode)
-        Log.d("PUI", kissEmojiString)
-        tvKiss.text = kissEmojiString
-
         val angryEmojiUnicode = 0x1F624
-        val angryEmojiString = getEmoji(angryEmojiUnicode)
-        Log.d("PUI", angryEmojiString)
-        tvAngry.text = angryEmojiString
-
         val fineEmojiUnicode = 0x1F62A
-        val fineEmojiString = getEmoji(fineEmojiUnicode)
-        Log.d("PUI", fineEmojiString)
-        tvFine.text = fineEmojiString
-
-
-        val winkEmojiUnicode = 0x1F609
-        val winkEmojiString = getEmoji(winkEmojiUnicode)
-        Log.d("PUI", winkEmojiString)
-        tvWink.text = winkEmojiString
-
-
-        val winkEmojiUnicode = 0x1F609
-        val winkEmojiString = getEmoji(winkEmojiUnicode)
-        Log.d("PUI", winkEmojiString)
-        tvWink.text = winkEmojiString
+        val shockEmojiUnicode = 0x1F631
+        val shyEmojiUnicode = 0x1F648
+        val earthEmojiUnicode = 0x1F30F
+        val pumpkinEmojiUnicode = 0x1F383
+        val cryingEmojiUnicode = 0x1F62D
+        val oneEyeTearEmojiUnicode = 0x1F625
+        val movieEmojiUnicode = 0x1F3AC
+        val hundredEmojiUnicode = 0x1F4AF
+        val fireEmojiUnicode = 0x1F525
+        val sleepingEmojiUnicode = 0x1F634
+        val heartEmojiUnicode = 0xFE0F;
     }
 
     private fun getEmoji(unicode: Int): String {
